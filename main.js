7f06fa5dfec5f47905c236bf1a600913dca795ba
AlysonRTY <variationsforpiano@gmail.com>
2025-02-20 16:17:50 +0100

    thursday commit

--- main.js
+++ main.js
@@ -16,14 +16,26 @@ console.log(data);
 //     td2.innerHTML = data[i].type
 //     td3.innerHTML = data[i].attribute
 //     td4.innerHTML = data[i].desc
-//     td5.innerHTML = ("src", data[i].card_images.image_url)  //need to be in src
+//     // td5.innerHTML = ("src", data[i].card_images.image_url)  //need to be in src
 
 //     tr.appendChild(td1)
 //     tr.appendChild(td2)
 //     tr.appendChild(td3)
 //     tr.appendChild(td4)
-//     tr.appendChild(td5)
+//     // tr.appendChild(td5)
 
 //     tbody.appendChild(tr)
     
+// }
+
+const yugiohCards = data
+
+const arr = yugiohCards.map((yugiohCard) => {
+    console.log(yugiohCard)
+    const name = yugiohCard.name
+    return name;
+})
+console.log(arr);
+// for (let i = 0; i < dataNames.length; i++){
+//     console.log(dataNames[i]);
 // }
